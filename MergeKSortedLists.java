dd2853dfb59b8b9afbc1fbec8a61909abfd4f761
Shruti Mahajan <47750477+mahajanshruti2509@users.noreply.github.com>
2020-06-03 23:16:45 -0500

    Merge K Sorted Lists

---
+++ MergeKSortedLists.java
@@ -0,0 +1,44 @@
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(ListNode head : lists) {
+            while(head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while(!minHeap.isEmpty()) {
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
