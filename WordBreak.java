d69328426c49595c0c303f8c2454882178634a52
Shruti Mahajan <47750477+mahajanshruti2509@users.noreply.github.com>
2020-07-12 17:51:07 -0500

    Memoization word break

--- WordBreak.java
+++ WordBreak.java
@@ -22,19 +22,41 @@ Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 
 */
+
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet=new HashSet(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        return dp[s.length()];
+        Set<String> wordDictSet = new HashSet(wordDict);
+        Map<String, Boolean> memoMap = new HashMap<>();
+        return dfs(s, wordDictSet, memoMap);
     }
+    
+    private boolean dfs(String s, Set<String> set, Map<String, Boolean> memoMap) {
+        int size = s.length();
+        // base case
+        if (size == 0) {
+          return true;
+        }
+
+        if(memoMap.containsKey(s)) {
+          return memoMap.get(s);
+        }
+
+        //else check for all words
+        for (int i = 1; i <= size; i++)
+        {
+          // Now we will first divide the word into two parts ,
+          // the prefix will have a length of i and check if it is
+          // present in dictionary ,if yes then we will check for
+          // suffix of length size-i recursively. if both prefix and
+          // suffix are present the word is found in dictionary.
+
+          if (set.contains(s.substring(0, i)) && dfs(s.substring(i, size), set, memoMap)) {
+            memoMap.put(s.substring(i, size), true);
+            return true;
+          }
+        }
+        // if all cases failed then return false
+        memoMap.put(s, false);
+        return false;
+  }
 }
